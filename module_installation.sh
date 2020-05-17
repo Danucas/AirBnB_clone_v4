c17abe02c4685b3107b9940821db3d8dfc5d4cb3
danrocus1994 <danrodcastillo1994@gmail.com>
2020-05-17 18:05:45 -0500

    installation modules

--- module_installation.sh
+++ module_installation.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-sudo apt-get install python3-mysqldb
 
+sudo apt-get install python3-mysqldb
+sudo pip3 install SQLAlchemy
+sudo pip3 install flask
+sudo pip3 install requests
+sudo pip3 install flask_cors
+sudo pip3 install flasgger
+echo 'deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7-dmr' | sudo tee -a /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install mysql-server-5.7
 # sudo pip3 install MySQL-python
 # sudo pip3 install MySQL-python-connector
